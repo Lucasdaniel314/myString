a91801ba0249d88446c5c73b82c8b0eb123af3ee
Lucas Daniel <Lucasdaniel314>
2023-09-23 13:23:08 -0300

    adding the types and changing some functions for them

--- myString.h
+++ myString.h
@@ -2,45 +2,106 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef enum {
+    LITTLE,
+    MEDIUM,
+    BIG,
+    GIANT
+} stringType;
+
 typedef struct string {
     char *value;
+    int len;
+    stringType type;
 } string;
 
-void createString(string *x) {
-    x->value = malloc(255 * sizeof(char));
-}
-
-char* stringToArray(string x) {
-    if (x.value == NULL) {
-        exit(1);
+string createString(stringType type, char initValue[]) {
+    string x;
+    switch (type) {
+        case LITTLE:
+            x.value = malloc(255 * sizeof(char));
+            x.type = type;
+            break;
+        case MEDIUM:
+            x.value = malloc(500 * sizeof(char));
+            x.type = type;
+            break;
+        case BIG:
+            x.value = malloc(1000 * sizeof(char));
+            x.type = type;
+            break;
+        case GIANT:
+            x.value = malloc(2000 * sizeof(char));
+            x.type = type;
+            break;
+        default:
+            printf("invalid type");
+            exit(1);
     }
-    return x.value;
+    if (initValue != NULL) {
+        if (arrayCharSize(initValue) > valueOf(type)) {
+            exit(1);
+        }
+        strcpy(x.value, initValue);
+        x.len = arrayCharSize(initValue);
+    } else {
+        x.len = 0;
+    }
+    return x;
 }
 
 void setCharAt(int i, string x, char newChar) {
     x.value[i] = newChar;
 }
 
-string inputString(char message[], int len) {
-    string strForReturning;
-    createString(&strForReturning);
-    if (len > 255) {
-        strForReturning.value = realloc(strForReturning.value, len * sizeof(char));
-    }
-    char value[len];
+string inputString(char message[], stringType type) {
+    string x = createString(type, NULL);
+    char value[valueOf(type)];
     printf("%s", message);
-    fgets(value, len, stdin);
+    fgets(value, valueOf(type), stdin);
     value[strlen(value) - 1] = '\0';
-    strcpy(strForReturning.value, value);
-    return strForReturning;
+    if (arrayCharSize(value) > valueOf(type)) {
+        exit(1);
+    }
+    x.len = arrayCharSize(value);
+    strcpy(x.value, value);
+    return x;
+}
+
+int valueOf(stringType type) {
+    switch (type) {
+        case LITTLE:
+            return 255;
+            break;
+        case MEDIUM:
+            return 500;
+            break;
+        case BIG:
+            return 1000;
+            break;
+        case GIANT:
+            return 2000;
+            break;
+    }
+    exit(1);
 }
 
 //print array of characters
-void printa(char *message) {
+void printa(char message[]) {
     printf("%s\n", message);
 }
 
 //print string
 void prints(string message) {
     printf("%s\n", message.value);
+}
+
+int size_tToInt(size_t x) {
+    return (int) x;
+}
+
+//error in this function
+/* when i input "yes", and i call this function to get the "yes" size, the returning value is 8, why 8? */
+int arrayCharSize(char a[]) {
+    return sizeof(a) / sizeof(a[0]);
 }
